26c3267ffa3be00eb41ca86bf84395cb4b989ca0
Ellison Leão <ellisonleao@gmail.com>
2016-11-27 18:18:03 -0200

    adding robots

--- yellowpages/urls.py
+++ yellowpages/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),
     url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     url(r'^admin/', admin.site.urls),
     url(r'^captcha/', include('captcha.urls')),
+    url(r'^robots\.txt$', TemplateView.as_view(
+        template_name='catalog/robots.txt',
+        content_type='text/plain')),
     url(r'^', include('catalog.urls')),
 ]
